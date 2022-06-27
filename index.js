9dd1ce16990b9bb481619b07e9b30033027b6f4d
William Lowrimore <92553989+wlowrimore@users.noreply.github.com>
2022-06-27 17:00:39 -0500

    completes first presentation of landingPage

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 $(document).ready(function () {
   var galleryImage = $(".gallery").find("img").first();
   var images = [
+    "./assets/images/eclipse.jpg",
     "./assets/images/bw_country-guy.jpg",
-    "./assets/images/Gadd.jpg",
+    "./assets/images/studio.jpg",
+    "./assets/images/kiss-cover.jpg",
     "./assets/images/r_b.jpg",
-    "./assets/images/drummer-hair.jpg",
-    "./assets/images/Peart.jpg"
+    "./assets/images/courtyard.jpg",
+    "./assets/images/Gadd.jpg",
+    "./assets/images/hotel-suite.jpg",
+    "./assets/images/Peart.jpg",
+    "./assets/images/synergy.jpg",
+    "./assets/images/zztop.jpg",
+    "./assets/images/jbeck.jpg",
+    "./assets/images/nash-night.jpg",
+    "./assets/images/fishbone-cover.jpg",
+    "./assets/images/arch.jpg",
+    "./assets/images/modern-drummer.jpg",
+    "./assets/images/hotel-room.jpg",
   ];
 
   var i = 0;
